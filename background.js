90cc9f4e7755122d8e7d559ddbb25239f333599d
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-31 13:52:57 +0900

    style: format by prettier

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-﻿chrome.runtime.onInstalled.addListener(async function () {
+﻿chrome.runtime.onInstalled.addListener(async () => {
   const tabs = await chrome.tabs.query({ url: ["https://www.youtube.com/*"] });
-  tabs.forEach(tab => insertCSS(tab));
+  tabs.forEach((tab) => {
+    insertCSS(tab);
+  });
 });
 
 function insertCSS(tab) {
